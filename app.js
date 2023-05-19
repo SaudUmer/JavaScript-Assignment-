87059a3b3f43c8cad682c5634e68937b85ba1cdb
SaudUmer <saudmamdani827@gmail.com>
2023-05-19 18:00:20 +0500

    First commit

---
+++ app.js
@@ -0,0 +1,358 @@
+//////////////////////chap01///////////////////////////////
+
+// alert("Muhammad");
+// alert("Saud");
+// alert("saud123@gmail.com");
+// alert("0322-2513800");
+// alert("password");
+////////////////////////////////////////////////////////////
+// alert("You are Learning javaScript");
+// /////////////////////////////////////////
+// alert("#Pakistan Zindabad");
+
+
+/////////////////////chap02//////////////////////////
+// var a;
+// /////////////////////////////////////
+// var a = 10;
+// //////////////////////////////////
+// var TeamName = "Team Warriors";
+// alert(TeamName);
+// /////////////////////////////////
+// var b = "Charlie";
+// var b = "Saud";
+
+
+////////////////// chap03///////////////////
+// var caseQty = 144;
+// var num = "9"
+// var rslt = 2+3;
+// console.log(rslt)
+// var merchTotal = 100;
+// var shipingCharge = 10;
+// var orderTotal = merchTotal+shipingCharge;
+// console.log(orderTotal)
+// var num = 2;
+// num = 3+5;
+// console.log(num)
+
+
+////////////////////////chap04///////////////////////
+// // var product_cost =3.45;
+// var nameOfband =;
+// var abc = 3;
+// var firstName = "anas"
+// var lastName = "ajaz"
+// var fullNname = firstName+ " "+ lastName;
+// console.log(fullNname)
+// var abc =legal;
+// var $abc =legal
+// var 123 = illegal;
+
+///////////////////////////chap05/////////////////////////////////
+// var rslt = 5%2;num
+// console.log(rslt)
+// var num = 20%6
+// console.log(num)
+// var largeNum = 1000*2000;
+// console.log(largeNum)
+
+////////////////////////chap06///////////////////////////
+// var x = 0;
+// x++;
+// alert(x);
+// ////////////////////
+
+// var y = 200;
+// y--;
+// alert(y);
+// ///////////////////////
+
+// var m = 99;
+// var n = m++;
+// alert(n);
+
+// /////////////////////////
+
+// var m = 201;
+// var n = --m;
+// alert(n);
+
+// ////////////////////////////// 
+// var p = 20;
+// p--;
+// var NewNum = p;
+// alert(NewNum);
+
+
+///////////////////////chap07////////////////////////
+
+// // // // var calculatedNum = 2+(2*6) //ans is 14
+// // // // alert(calculatedNum); 
+// // // // //////////////////
+// // // // var calculatedNum = (2+2)*6
+// // // // alert(calculatedNum); //ans is 24
+// // // // ////////////////////////
+// // // // var calculatedNum = (2+2)*(4+2)
+// // // // alert(calculatedNum); //ans is 24
+// // // // /////////////////////
+// // // // var calculatedNum = ((2+2)*4) + 2;
+// // // // alert(calculatedNum); //ans is 18
+// // // ///////////////////
+// // // var a= 2+2*4+10;
+// // // alert ("56");
+// // // /////////////////////
+// // // var a= 2+2*4+10;
+// // // alert (a);
+
+/////////////////chap08///////////////////////////////
+
+// // var num = "2" + "2";
+// // alert(num); // ans is 22
+// // ///////////////////
+// // var message = ("hello," + "dolly");
+// // alert(message) // ans is hello,dolly
+// // /////////
+// // alert("33" + 3); //ans is 333
+// // /////////////
+// var a = "Pakistan";
+// var b ="zindabad";
+// alert(a + b);
+
+// ///////////////////
+// var a= 5;
+// var b=5;
+// var c = a+b;
+// alert(c);
+
+
+///////////////////////chap 09////////////////////////
+
+// var name = prompt("enter your name");
+ //////////////
+// var country = prompt("country?","china")
+//////////////
+//var yourName =("enter ypur name");
+//////////////////////
+// var a = "hello sam";
+// var b= "charlie";
+
+//////////Chap 11//////////////////////
+// var a = 2;
+// var b = 3;
+// if(a==b){
+//     alert("equal")
+// }
+// else{
+//     alert("unequal")
+
+// }
+// var a = 2;
+// var b = 3;
+// if(a>=b){
+//     alert("a is greater")
+// }
+// else{
+//     alert("b is greater")
+
+// }
+
+
+// var a = 2;
+// if(a!2){
+//     
+// }
+// var a = 2;
+// var b = 3;
+// if(a!=b){
+// alert("congrates")
+// }
+// var firstname =prompt("name")
+// var lastName= "ajaz"
+// if(firstname!=lastName){
+// alert("no match")
+// }
+
+
+
+/////////////// Chap 12 ////////////////////////////////
+
+// var a = 2;
+// var b = 3
+// if (a >= b) {
+//     alert("a is greater or equal")
+// }
+// else {
+//     alert("a is not greater or equal")
+// }
+// var mark = prompt("enter mark")
+// var percentage = (mark / 100) * 100
+// if (percentage >= 70 && percentage <= 79) {
+//     alert("grade A")
+// }
+// else if (percentage >= 80 && percentage <= 100) {
+//     alert("grade A++")
+// }
+// else {
+//     alert("fail")
+// }
+// var a = 7
+// if (a === 10) {
+//     alert("correct")
+// } else {
+//     alert("correct value of a:" + " " + a)
+// }
+// var city = prompt("enter city")
+// if (city == "karachi") {
+//     alert("yes it is karachi")
+// } else if (city == "lahore") {
+//     alert("yes it is lahore")
+// }
+// else {
+//     alert(" it is other city")
+// }
+
+////////////////////Chap 13////////////////////////
+
+// var a=3
+// var b=3
+// var c=6
+// var d=6
+// if ( a==b && c==d){
+//     alert ("a is eqal to b and c is equal to d")
+
+// }else {
+//     alert("your statement is false")
+// }
+
+///////////////
+// var a=3
+// var b=3
+// var c=6
+// var d=5
+// if ( a==b && c!=d){
+//     alert ("a is eqal to b and c is not equal to d")
+
+// }else {
+//     alert("your statement is false")
+// }
+////////////////
+// var name1="hamza"
+// var name1="arslan"
+// var age=60
+// if (name1="hamza" || var name1="arslan"  && age == 60){
+//     alert ("you are eligible")
+
+// }else {
+//     alert("not eligible")
+// }
+////////////////////
+// var a =5
+// var b=10
+// if(a<b || a>b){
+//     alert("a is not equal to b")
+// }
+/////////////
+// var fname= prompt("enter your firstname")
+// var lname= prompt("enter your lastname")
+// if(fname== lname){
+//     alert("your first and last names should not equal")
+// }
+
+
+////////////// Chap 14 /////////////////////
+// var pswd = prompt("enter passward")
+// if (pswd != "") {
+//     if (pswd < 5) {
+//         alert("passward must be greater than 5")
+//     }
+//     else {
+//         alert("ok")
+//     }
+// }
+// else {
+//     alert("please enter passward")
+// }
+// var a = 1;
+// var c = "max";
+// if (a === 1) {
+//     if (c === "min") {
+//         alert("ok")
+//     }
+//     else if (c === "max") {
+//         alert("ok")
+//     }
+
+// }
+// var a = 2;
+// var b = 2;
+// if (a = b) {
+//     if (a <= b) {
+//         document.write("pass")
+//     }
+// }
+// else {
+//     alert("test pass")
+// }
+
+
+/////////////////////////Chap 15////////////////////
+// var arr = []
+// console.log(arr);
+
+// //////////////////////////
+// var arr = ["Array"];
+// console.log(arr);
+// ///////////////////
+// var alphabet = ["h", "i", "j", "k"];
+// alert(2);
+// ////////////////
+// var alphabet = ["h", "i", "j", "k", "l", "m", "n", "o"];
+// console.log(alphabet.length);
+// /////////////////////////////
+
+// ///////////////////////////Chap16/////////////////
+// var string = ["Javascript"]
+// string.push("HTML");
+// ///////////////////////////
+// var alphabet = ["h", "i", "j", "k"];
+// alphabet.pop();
+// ///////////////////////
+// var alphabet = ["h", "i", "j", "k"];
+// alphabet.push("l");
+// ////////////////////////
+// var alphabet = ["h", "i", "j", "k"];
+// alphabet.push("l");
+// /////////////////////////////
+// var Sizes = ["Sm", "Lg", "Xl", "XXL"];
+// alphabet.shift();
+// ///////////////////////////////////
+// var Sizes = ["Sm", "Lg", "Xl", "XXL"];
+// alphabet.shift();
+// //////////////////////////
+// var Sizes = ["Sm", "Lg", "Xl", "XXL"];
+// alphabet.unshift(1, 2, 3);
+// ///////////////////////////
+// var pets = ["dog","cat","ox","duck","frog", "cow"]
+// pets.slice(3, 5);
+/////////////////////////////////
+
+///////////////////////Chap 17 to Chap 20////////////////
+
+// for (i = 0; i <= 10; i++) {
+//     console.log(i)
+// }
+// for (i = 0; i <= 12; i++) {
+//     console.log(i)
+// }
+// for(var i=0; i<=4; i++){
+
+// }
+// for(j=0; j<=100; j++){
+
+// }
+// for (i = 3; i > 3; i++) {
+
+//  }
+
+/////////////////////////////////////
